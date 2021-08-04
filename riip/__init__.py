04d0f5fa7887bca5e663cb703e10a45258d95014
joaquin <j>
2021-08-03 22:21:21 -0700

    added tox github actions

--- riip/__init__.py
+++ riip/__init__.py
@@ -1,3 +1,15 @@
+from riip import material
+
+from riip.dataframe_operations import RiiDataFrame, load_csv
+from riip.material import Material
+
+__all__ = [
+    "Material",
+    "RiiDataFrame",
+    "load_csv",
+    "material",
+]
+
 __author__ = "Munehiro Nishida"
 __version__ = "0.1.0"
 __license__ = "GPLv3"
