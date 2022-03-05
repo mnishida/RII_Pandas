4bc96004d5a94df68d85a3abfed2a7c893013cd3
mnishida <mnishida@hiroshima-u.ac.jp>
2022-03-05 10:24:10 +0900

    v.0.6.12

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 from recommonmark.transform import AutoStructify
 
 project = "RII_Pandas"
-version = "0.6.11"
+version = "0.6.12"
 copyright = "2021, Munehiro Nishida"
 author = "Munehiro Nishida"
 
