f1389c134890c2920bed3e282948285214f50d9a
mnishida <mnishida@hiroshima-u.ac.jp>
2026-05-31 01:43:40 +0900

    v0.8.2

--- README.md
+++ README.md
@@ -92,4 +92,4 @@ $ conda uninstall riip
 - gitpython
 
 ## Version
-0.8.1
+0.8.2
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ ext_modules.append(e)
 
 setup(
     name="riip",
-    version="0.8.1",
+    version="0.8.2",
     url="https://github.com/mnishida/RII_Pandas",
     license="MIT",
     author="Munehiro Nishida",
