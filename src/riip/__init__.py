a9014dab95d0677ff2f549c6268e388b1a019d91
mnishida <mnishida@hiroshima-u.ac.jp>
2026-05-31 01:12:24 +0900

    v0.8.0

--- src/riip/__init__.py
+++ src/riip/__init__.py
@@ -1,11 +1,6 @@
 from .dataframe import RiiDataFrame
 from .material import Material
 
-# __all__ = [
-#     "Material",
-#     "RiiDataFrame",
-# ]
-
 __author__ = "Munehiro Nishida"
 __version__ = "0.8.0"
 __license__ = "MIT"
